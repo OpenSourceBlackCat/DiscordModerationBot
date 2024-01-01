54d5c0c8c72dd72af2767a3b3ca854740e581e02
OpenSourceBlackCat <amey992004@gmail.com>
2024-01-02 05:12:02 +0530

    Welcome Function Is Completed (Basic)

--- DiscordBot/Welcome.js
+++ DiscordBot/Welcome.js
@@ -4,6 +4,7 @@ module.exports = WelcomeBot = async(DiscordBot, Config) =>{
         const welcomeEmbed = new EmbedBuilder()
         .setTitle(Config.WelcomeTitle)
         .setDescription(Config.WelcomeDescription)
+        .setColor(Config.WelcomeColor)
         .setImage(Config.WelcomeImage)
         .setFooter({text: ctx.guild.name, iconURL: (ctx.guild.icon ? ctx.guild.icon : Config.WelcomeServerIcon)})
         var welcomeChannel = await ctx.guild.channels.fetch(Config.WelcomeChannel)
